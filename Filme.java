b81ae14c142ad42255291eeda5b61eb7a45573d2
daviPeter07 <davipetersondev173@gmail.com>
2025-10-28 19:54:23 -0400

    feat: up AULA2

--- Filme.java
+++ Filme.java
@@ -4,10 +4,42 @@ public class Filme {
   int duracao;
   int anoLancamento;
 
-  void exibirInfo() {
-    System.out.println("Título: " + titulo);
-    System.out.println("Gênero: " + genero);
-    System.out.println("Duração: " + duracao + " minutos");
+  // Getter e Setter de titulo
+  public String getTitulo() {
+    return titulo;
+  }
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  // Getter e Setter de genero
+  public String getGenero() {
+    return genero;
+  }
+  public void setGenero(String genero) {
+    this.genero = genero;
+  }
+
+  // Getter e Setter de duracao
+  public int getDuracao() {
+    return duracao;
+  }
+  public void setDuracao(int duracao) {
+    this.duracao = duracao;
+  }
+
+  // Getter e Setter de anoLancamento
+  public int getAnoLancamento() {
+    return anoLancamento;
+  }
+  public void setAnoLancamento(int anoLancamento) {
+    this.anoLancamento = anoLancamento;
+  }
+
+  public void exibirInfo() {
+    System.out.println("Titulo: " + titulo);
+    System.out.println("Genero: " + genero);
+    System.out.println("Duracao: " + duracao);
     System.out.println("Ano de lançamento: " + anoLancamento);
   }
 }
--- Principal.java
+++ Principal.java
@@ -1,20 +1,15 @@
 public class Principal {
   public static void main(String[] args) {
     Filme filme1 = new Filme();
-    Filme filme2 = new Filme();
 
-    filme1.titulo = "Matrix";
-    filme1.genero = "Ficção Científica";
-    filme1.duracao = 136;
-    filme1.anoLancamento = 1999;
+    //definicao de Setters
+    filme1.setTitulo("Matrix");
+    filme1.setGenero("Ficção Científica");
+    filme1.setDuracao(136);
+    filme1.setAnoLancamento(1999);
 
-    filme2.titulo = "O Rei Leão";
-    filme2.genero = "Animação";
-    filme2.duracao = 118;
-    filme2.anoLancamento = 1994;
-
+    //
     filme1.exibirInfo();
-    System.out.println("--------------------------");
-    filme2.exibirInfo();
+    System.out.println("Titulo do filme: " + filme1.getTitulo());
   }
 }
